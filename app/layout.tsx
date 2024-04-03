e7121438aa2a3ab58be43dfde377e5f4cb6869ae
muhammadatiqueqayum <atiqueqayum@gmail.com>
2024-04-04 03:52:33 +0500

    into section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import header from "@/components/header";
+import Header from "@/components/header";
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
